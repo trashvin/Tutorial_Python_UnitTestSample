5693cfb1f0de47f8270f9f6c354395f81e6aaf32
Trashvin <m3lles@gmail.com>
2017-05-28 21:24:39 +0800

    Corrected division method

--- Modules/Calculator/main.py
+++ Modules/Calculator/main.py
@@ -14,7 +14,7 @@ class Calculator():
         return num1 * num2
 
     def divide(self,num1,num2):
-        return num1/num2 + 1
+        return num1/num2
 
 if __name__ == "__main__":
     myApp = Calculator()
